14c48b4eb89351b44106d790a7fc73ecb7b6e73a
drvkcloud <drvkcloud@gmail.com>
2021-06-05 16:32:50 +0530

    Bolierplate code

---
+++ src/main/java/com/cr/dev/CustomRigs/CustomRigsApplication.java
@@ -0,0 +1,13 @@
+package com.cr.dev.CustomRigs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomRigsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomRigsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cr/dev/CustomRigs/CustomRigsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cr.dev.CustomRigs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomRigsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
